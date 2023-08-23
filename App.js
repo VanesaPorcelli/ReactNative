84efa2e2928ea9b8e3db227b60230abeb722ab31
Vanesa Porcelli <vanesaporcelli@gmail.com>
2023-08-23 19:12:57 -0300

    Iniciando React Native

---
+++ App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hola Coder! Soy Vanesa Porcelli</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#e0e0e0',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
